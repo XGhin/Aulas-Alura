2c20f8889ed60346fd26491e3c45b470011ec614
XGhin <gabrieljdalposso@gmail.com>
2023-07-25 10:56:13 -0300

    Começando estudo de Orientação a Objeto

---
+++ Orientação Objeto Python/teste.py
@@ -0,0 +1,12 @@
+def criar_conta(numero, titular, saldo, limite):
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
+
+def sacar(conta, valor):
+    conta["saldo"] -= valor
+
+def depositar(conta, valor):
+    conta["saldo"] += valor
+
+def extrato(conta, saldo):
+    print("seu saldo é {}".format(conta["saldo"]))

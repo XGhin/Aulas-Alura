5062d63849f912578daac501802d974bf5d46921
XGhin <132624091+XGhin@users.noreply.github.com>
2023-08-01 17:21:14 -0300

    Criando MODELO

---
+++ Orientação Objeto Python/OO p2/modelo.py
@@ -0,0 +1,35 @@
+class Programa():
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        self._nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self._likes = 0
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        self._nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self._likes = 0
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+print(vingadores.nome)
+
+atlanta = Serie('atlanta', 2018, 2)
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')

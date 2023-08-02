1642d58fa0996d283171863e74f8f4f67a952278
XGhin <gabrieljdalposso@gmail.com>
2023-08-02 09:30:26 -0300

    Reorganizando Arquivos

--- Orientação Objeto Python/Desafios/Operadores.py
+++ Orientação Objeto Python/Desafios/Operadores.py
@@ -1,4 +1,4 @@
-class Operadores:
+class Operacoes:
     def somar():
         n1 = int(input("Digite o primeiro valor: "))
         n2 = int(input("Digite o segundo valor: "))
@@ -23,24 +23,3 @@ class Operadores:
         n1 = int(input("Digite o primeiro valor: "))
         n2 = int(input("Digite o segundo valor: "))
         print("O resultado da potencia foi: {}".format(n1**n2))
-
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX CODIGO XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-print("Bem vindo a Calculadora (2.0)")
-
-print("(+) Soma\n(-) Subtração\n(*) Multiplicação\n(/) Divisão\n(^) Elevado")
-operador = input("Qual operação você deseja fazer?")
-
-if (operador == "+"):
-    Operadores.somar()
-elif (operador == "-"):
-    Operadores.subtrair()
-elif (operador == "*"):
-    Operadores.multiplicar()
-elif (operador == "/"):
-    Operadores.dividir()
-elif (operador == "^"):
-    Operadores.potencia()
-    
-    
-    

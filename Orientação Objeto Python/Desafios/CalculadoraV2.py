d20f7bc8302f2096f4b26398267c621c352f88b7
gabriel <gabrieljdalposso@gmail.com>
2023-08-01 22:08:09 -0300

    Criando CalculadoraV2.py (usando Funções)

---
+++ Orientação Objeto Python/Desafios/CalculadoraV2.py
@@ -0,0 +1,29 @@
+print("Bem vindo a Calculadora (2.0)")
+
+print("(+) Soma\n(-) Subtração\n(*) Multiplicação\n(/) Divisão\n(^) Elevado")
+operador = input("Qual operação você deseja fazer?")
+
+def somar():
+    n1 = int(input("Digite o primeiro valor: "))
+    n2 = int(input("Digite o segundo valor: "))
+    print("O resultado da soma foi: {}".format(n1+n2))
+
+def somar():
+    n1 = int(input("Digite o primeiro valor: "))
+    n2 = int(input("Digite o segundo valor: "))
+    print("
+
+def somar():
+    n1 = int(input("Digite o primeiro valor: "))
+    n2 = int(input("Digite o segundo valor: "))
+    print("O resultado da soma foi: {}".format(n1+n2))
+
+def somar():
+    n1 = int(input("Digite o primeiro valor: "))
+    n2 = int(input("Digite o segundo valor: "))
+    print("O resultado da soma foi: {}".format(n1+n2))
+
+def somar():
+    n1 = int(input("Digite o primeiro valor: "))
+    n2 = int(input("Digite o segundo valor: "))
+    print("O resultado da soma foi: {}".format(n1+n2))

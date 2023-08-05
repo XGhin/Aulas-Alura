275ed7824d2a8dde109b82f4b23a1af563198339
XGhin <gabrieljdalposso@gmail.com>
2023-08-05 15:52:14 -0300

    Começando Projeto de Estoque em Python

---
+++ Orientação Objeto Python/Desafios/Estoque.py
@@ -0,0 +1,23 @@
+class Estoque:
+    itens = []
+
+    def __init__(self, item, quantidade, valor):
+        self._item = item
+        self._quantidade = quantidade
+        self._valor = valor
+        Estoque.itens.append(item)
+
+    def adicionar(self, quantidade):
+        self._quantidade += quantidade
+
+    def new_price(self, valor):
+        self._valor = valor
+
+    @property
+    def estoque(self):
+        return self._quantidade
+
+    @property
+    def valor(self):
+        print("Item: {}\nValor: R${} Reais".format(self._item, self._valor))
+
